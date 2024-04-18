78b6089197718e88980299e93d743c5dc2935cb8
dualex23 <alexinc2.0@gmail.com>
2024-04-18 13:36:11 +0300

    increment is completed v1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,20 +8,24 @@ import (
 
 	"github.com/dualex23/go-url-shortener/internal/app/config"
 	"github.com/dualex23/go-url-shortener/internal/app/handler"
+	"github.com/dualex23/go-url-shortener/internal/app/middleware"
+	"github.com/dualex23/go-url-shortener/internal/app/utils"
 )
 
-
-
 func main() {
+	utils.InitLogger()
+	defer utils.GetLogger().Sync()
+
 	appConfig := config.AppParseFlags()
-	
+
 	r := chi.NewRouter()
-	
+
 	shortenerHandler := handler.NewShortenerHandler(appConfig.BaseURL)
 
+	r.Use(middleware.WithLogging)
 	r.Post("/", shortenerHandler.MainHandler)
-	r.Get("/{id}",shortenerHandler.GetHandler)
-	
+	r.Get("/{id}", shortenerHandler.GetHandler)
+
 	err := http.ListenAndServe(appConfig.ServerAddr, r)
 	if err != nil {
 		log.Fatal(err)
