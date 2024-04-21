2e52f7af16d62eed61e64cf477a086f4d5d19b2e
dualex23 <alexinc2.0@gmail.com>
2024-04-21 23:49:08 +0300

    fix error filePath v4

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -18,16 +18,13 @@ type App struct {
 func AppParseFlags() *App {
 	var appConfig App
 
-	baseDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	dataDir := filepath.Join(baseDir, "data", "tmp")
-
 	appConfig.ServerAddr = "localhost:8080"
 	appConfig.BaseURL = "http://localhost:8080"
-	defaultFilePath := filepath.Join(dataDir, "short-url-db.json")
+	defaultFilePath := "/tmp/short-url-db.json"
 
 	flag.StringVar(&appConfig.ServerAddr, "a", appConfig.ServerAddr, "Адрес запуска HTTP-сервера")
 	flag.StringVar(&appConfig.BaseURL, "b", appConfig.BaseURL, "Базовый адрес результирующего сокращённого URL")
-	flag.StringVar(&appConfig.FileStoragePath, "f", defaultFilePath, "Путь к файлу данных")
+	flag.StringVar(&appConfig.FileStoragePath, "f", defaultFilePath, "Имя файла данных без пути")
 	flag.Parse()
 
 	err := godotenv.Load()
@@ -44,7 +41,8 @@ func AppParseFlags() *App {
 	if envFilePath := os.Getenv("FILE_STORAGE_PATH"); envFilePath != "" {
 		appConfig.FileStoragePath = envFilePath
 	} else {
-		appConfig.FileStoragePath = defaultFilePath
+		currentDir, _ := os.Getwd()
+		appConfig.FileStoragePath = filepath.Join(currentDir, appConfig.FileStoragePath)
 	}
 
 	return &appConfig
