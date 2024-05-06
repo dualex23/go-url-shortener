c0f54743244b8518ce9bcb40caa6ce14b74b7204
dualex23 <alexinc2.0@gmail.com>
2024-05-06 21:32:44 +0300

    fix6

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -29,7 +29,7 @@ func AppParseFlags() *App {
 	flag.StringVar(&appConfig.DataBaseDSN, "d", appConfig.DataBaseDSN, "DB настройки")
 	flag.Parse()
 
-	err := godotenv.Load(".env")
+	err := godotenv.Load("cmd/shortener/.env")
 	if err != nil {
 		logger.GetLogger().Error("Warning: .env file not found or error loading .env file")
 	}
